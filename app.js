2f734f42b63a066e6fd73e48a5a4eaa6c69e4175
daniarabykeev <daniarabykeev@gmail.com>
2023-02-10 17:10:47 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const cols = document.querySelectorAll(".col");
+
+document.addEventListener("keydown", (event) => {
+  if (event.code.toLowerCase() === "space") {
+    setRandomColors();
+  }
+});
+
+document.addEventListener("click", (event) => {
+  const type = event.target.dataset.type;
+
+  if (type === "lock") {
+    console.log(event.target);
+  }
+});
+
+function generateRandomColor() {
+  const hexCodes = "0123456789ABCDEF";
+  let color = "";
+  for (let i = 0; i < 6; i++) {
+    color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
+  }
+  return "#" + color;
+}
+
+function setRandomColors() {
+  cols.forEach((col) => {
+    const text = col.querySelector("h2");
+    const button = col.querySelector("button");
+
+    const color = chroma.random();
+
+    text.textContent = color;
+    col.style.background = color;
+
+    setTextColor(text, color);
+    setTextColor(button, color);
+  });
+}
+
+function setTextColor(text, color) {
+  const luminance = chroma(color).luminance();
+  text.style.color = luminance > 0.5 ? "black" : "white";
+}
+
+setRandomColors();
+
+// cwcewcwecffsgwgrwg
